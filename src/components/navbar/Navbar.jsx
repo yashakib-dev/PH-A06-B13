7f1b338956c07a8f940b2ecc31cabac41694a636
yashakib-dev <yashakib.dev@gmail.com>
2026-03-29 21:58:38 +0600

    Setup Done

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <>
+            <div>
+                <h2>DigiTools</h2>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Navbar;
